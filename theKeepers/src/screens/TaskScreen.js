9ef557c92415c9b404f80176b5f5a598328eb536
Sahana-Avantika <sahanaavantika7@gmail.com>
2024-06-24 19:22:25 +0800

    Update TaskScreen.js

--- theKeepers/src/screens/TaskScreen.js
+++ theKeepers/src/screens/TaskScreen.js
@@ -1,31 +1,27 @@
 import React from 'react';
-import { View, Text, SafeAreaView, TouchableOpacity, ImageBackground, TextInput, Alert, StyleSheet } from 'react-native';
+import { View, Text, SafeAreaView, TouchableOpacity, ImageBackground, FlatList, TextInput, StyleSheet } from 'react-native';
 import { addDoc, collection, Timestamp } from "firebase/firestore";
+import Feather from 'react-native-vector-icons/Feather';
 import { db } from '../firebase/config'; 
 import { useTasks } from '../contexts/TasksContext'; 
 import { useAuth } from '../contexts/AuthContext';
+import { Modal } from 'react-native-paper';
 
 export default function TaskScreen({ navigation }) {
   const [task, setTask] = React.useState("");
   const [date, setDate] = React.useState("");
+  const [popVisible, setPopVisibility] = React.useState(false);
   const { loggedInUser } = useAuth();
+  const { tasks, toggleTaskCompletion } = useTasks();
 
   const handleAddTask = async () => {
     if (!validateDate(date)) {
       alert('Please enter the date in yyyy-mm-dd format'); // check format
       return;
     }
-    
-  /*
-  const parsedDate = new Date(date);
-  if (isNaN(parsedDate.getTime())) {
-    alert('Invalid date. Please enter a valid date.'); dont use it, can take input s.a 1672
-    return;
-  }
-    */
 
     try {
-      const docRef = await addDoc(collection(db, "todo"), { //update to general id
+      const docRef = await addDoc(collection(db, "todo"), {
         email: loggedInUser?.email,
         title: task,
         completed: false, 
@@ -46,26 +42,79 @@ export default function TaskScreen({ navigation }) {
     return regex.test(date);
   };
 
+  const renderItem = ({ item }) => (
+    <View style={styles.taskContainer}>
+      <View style={styles.taskInfo}>
+        <Text style={styles.taskTitle}>{item.title}</Text>
+        <Text style={styles.taskDeadline}>{item.deadline.toDateString()}</Text>
+      </View>
+      <TouchableOpacity
+        style={styles.checkboxContainer}
+        onPress={() => toggleTaskCompletion(item.id, item.completed)}
+      >
+        {item.completed ? (
+          <Feather name="check-circle" size={24} color="green" />
+        ) : (
+          <Feather name="circle" size={24} color="black" />
+        )}
+        <Text style={[styles.itemText, item.completed && styles.completedTask]}>
+          {item.title}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+  
   return (
     <SafeAreaView style={styles.container}>
       <ImageBackground source={require('../assets/the_background.png')} resizeMode="cover" style={styles.image}>
-        <TextInput
-          style={styles.input}
-          placeholder="Task"
-          placeholderTextColor="#ffffff"
-          value={task}
-          onChangeText={(task) => setTask(task)}
+        <FlatList
+          data={Object.keys(tasks).map(key => ({ date: key, data: tasks[key] })).flatMap(item => item.data)}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+          contentContainerStyle={styles.taskList}
         />
 
-        <TextInput
-          style={styles.input}
-          placeholder="yyyy-mm-dd"
-          placeholderTextColor="#ffffff"
-          value={date}
-          onChangeText={(date) => setDate(date)}
-        />
-        <TouchableOpacity onPress={handleAddTask} style={styles.button}>
-          <Text style={styles.buttonText}>Add Task</Text>
+        <Modal
+          animationType='slide'
+          visible={popVisible}
+          onRequestClose={() => {
+            setPopVisibility(!popVisible);
+          }}
+        >
+          <View style={styles.modalView}>
+            <TextInput
+              style={styles.input}
+              placeholder="Task"
+              placeholderTextColor="#696969"
+              value={task}
+              onChangeText={(task) => setTask(task)}
+            />
+
+            <TextInput
+              style={styles.input}
+              placeholder="yyyy-mm-dd"
+              placeholderTextColor="#696969"
+              value={date}
+              onChangeText={(date) => setDate(date)}
+            />
+
+            <TouchableOpacity onPress={handleAddTask} style={styles.button}>
+              <Text style={styles.buttonText}>Add Task</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={[styles.button, styles.button]}
+              onPress={() => setPopVisibility(!popVisible)}
+            >
+              <Text style={styles.buttonText}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+
+        <TouchableOpacity
+          style={styles.addButton}
+          onPress={() => setPopVisibility(true)}
+        >
+          <Text style={styles.addButtonText}>+</Text>
         </TouchableOpacity>
       </ImageBackground>
     </SafeAreaView>
@@ -75,13 +124,6 @@ export default function TaskScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
   },
   image: {
     flex: 1,
@@ -89,7 +131,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     width: "100%",
     height: "100%",
-    margin: 0,
   },
   input: {
     width: "80%",
@@ -100,7 +141,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     paddingLeft: 8,
     paddingRight: 8,
-    color: "#ffffff",
+    color: "#000000",
     borderRadius: 15,
   },
   button: {
@@ -112,14 +153,90 @@ const styles = StyleSheet.create({
     height: 50,
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: "row",
   },
   buttonText: {
     fontSize: 20,
     fontWeight: "bold",
-    marginBottom: 5,
     color: '#fffff0',
     textAlign: 'center',
-  }
+  },
+  addButton: {
+    backgroundColor: "#302298",
+    width: 60,
+    height: 60, 
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    bottom: 30,
+    right: 30,
+    position: 'absolute',
+  },
+  addButtonText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: '#fffff0',
+    textAlign: 'center',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  taskList: {
+    width: "100%",
+    padding: 20,
+  },
+  taskContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#f9f9f9",
+    padding: 15,
+    marginVertical: 8,
+    borderRadius: 10,
+    width: "100%",
+  },
+  taskInfo: {
+    flex: 1,
+    color: "#000000",
+  },
+  taskTitle: {
+    fontSize: 18,
+    flex: 1,
+    color: "#000000",
+  },
+  taskDeadline: {
+    fontSize: 14,
+    color: "#888",
+    textAlign: 'right',
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  itemText: {
+    color: 'black',
+    fontSize: 17,
+    marginLeft: 10,
+    flex: 1,
+  },
+  completedTask: {
+    textDecorationLine: 'line-through',
+    color: 'gray',
+  },
 });
 
